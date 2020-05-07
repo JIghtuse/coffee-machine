a4b2d6c3bf3e62a7857eba1e7d4d4ecfef76f32c
Егоров Борис Владимирович <b.egorov@2gis.ru>
2020-05-07 19:06:32 +0700

    Stage 6/6: Brush your code

--- src/machine/Main.kt
+++ src/machine/Main.kt
@@ -1,10 +1,20 @@
 package machine
 
+enum class MachineState {
+    ChoosingAction,
+    ChoosingBeverage,
+    FillingWater,
+    FillingMilk,
+    FillingBeans,
+    FillingDisposableCups,
+    Stopped
+}
+
 class Beverage(
-        val waterMl: Int,
-        val milkMl: Int,
-        val beansG: Int,
-        val cost: Int
+    val waterMl: Int,
+    val milkMl: Int,
+    val beansG: Int,
+    val cost: Int
 )
 
 val espresso = Beverage(250, 0, 16, 4)
@@ -12,54 +22,95 @@ val latte = Beverage(350, 75, 20, 7)
 val cappuccino = Beverage(200, 100, 12, 6)
 
 class CoffeeMachine(
-        private var waterMl: Int,
-        private var milkMl: Int,
-        private var beansG: Int,
-        private var disposableCups: Int,
-        private var money: Int) {
+    private var waterMl: Int,
+    private var milkMl: Int,
+    private var beansG: Int,
+    private var disposableCups: Int,
+    private var money: Int) {
 
-    fun buyCoffee() {
+    private var state = MachineState.Stopped
+
+    private val chooseActionMessage = "Write action (buy, fill, take, remaining, exit): "
+
+    fun start() {
+        state = MachineState.ChoosingAction
+        print(chooseActionMessage)
+    }
+
+    fun stopped() = state == MachineState.Stopped
+
+    fun perform(action: String) {
+        state = when (state) {
+            MachineState.ChoosingAction -> when (action) {
+                "buy" -> MachineState.ChoosingBeverage
+                "fill" -> MachineState.FillingWater
+                "take" -> {
+                    takeMoney()
+                    MachineState.ChoosingAction
+                }
+                "remaining" -> {
+                    reportState()
+                    MachineState.ChoosingAction
+                }
+                "exit" -> MachineState.Stopped
+                else -> {
+                    println("Unknown action '$action'")
+                    MachineState.ChoosingAction
+                }
+            }
+            MachineState.ChoosingBeverage -> {
+                if (action != "back") {
+                    buyCoffee(action)
+                }
+                MachineState.ChoosingAction
+            }
+            MachineState.FillingWater -> {
+                waterMl += action.toInt()
+                MachineState.FillingMilk
+            }
+            MachineState.FillingMilk -> {
+                milkMl += action.toInt()
+                MachineState.FillingBeans
+            }
+            MachineState.FillingBeans -> {
+                beansG += action.toInt()
+                MachineState.FillingDisposableCups
+            }
+            MachineState.FillingDisposableCups -> {
+                disposableCups += action.toInt()
+                MachineState.ChoosingAction
+            }
+            MachineState.Stopped -> MachineState.Stopped
+        }
+
         println()
-        print("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu: ")
+        print(when (state) {
+            MachineState.ChoosingAction -> chooseActionMessage
+            MachineState.ChoosingBeverage -> "What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu: "
+            MachineState.FillingWater -> "Write how many ml of water do you want to add: "
+            MachineState.FillingMilk -> "Write how many ml of milk do you want to add: "
+            MachineState.FillingBeans -> "Write how many grams of coffee beans do you want to add: "
+            MachineState.FillingDisposableCups -> "Write how many disposable cups of coffee do you want to add: "
+            MachineState.Stopped -> ""
+        })
+    }
 
-        val input = readLine()!!
-
-        if (input == "back") return
-
-        when (input.toInt()) {
+    private fun buyCoffee(chosenCoffee: String) {
+        when (chosenCoffee.toInt()) {
             1 -> buy(espresso)
             2 -> buy(latte)
             3 -> buy(cappuccino)
+            else -> println("Unknown coffee $chosenCoffee")
         }
     }
 
-    fun fill() {
-        print("Write how many ml of water do you want to add: ")
-        val addWaterMl = readInt()
-
-        print("Write how many ml of milk do you want to add: ")
-        val addMilkMl = readInt()
-
-        print("Write how many grams of coffee beans do you want to add: ")
-        val addBeansG = readInt()
-
-        print("Write how many disposable cups of coffee do you want to add: ")
-        val addDisposableCups = readInt()
-
-        waterMl += addWaterMl
-        milkMl += addMilkMl
-        beansG += addBeansG
-        disposableCups += addDisposableCups
-    }
-
-
-    fun takeMoney() {
+    private fun takeMoney() {
         println()
         println("I gave you $$money")
         money = 0
     }
 
-    fun reportState() {
+    private fun reportState() {
         println()
         println("The coffee machine has:")
         println("$waterMl of water")
@@ -95,27 +146,13 @@ class CoffeeMachine(
     }
 }
 
-fun readInt(): Int {
-    return readLine()!!.toInt()
-}
-
-fun runMachineLoop() {
+fun main() {
     val machine = CoffeeMachine(400, 540, 120, 9, 550)
 
-    while (true) {
-        print("Write action (buy, fill, take, remaining, exit): ")
-        when (val action = readLine()!!) {
-            "buy" -> machine.buyCoffee()
-            "fill" -> machine.fill()
-            "take" -> machine.takeMoney()
-            "remaining" -> machine.reportState()
-            "exit" -> return
-            else -> println("Unknown action '$action'")
-        }
-        println()
-    }
-}
+    machine.start()
 
-fun main() {
-    runMachineLoop()
+    while (!machine.stopped()) {
+        val action = readLine()!!
+        machine.perform(action)
+    }
 }
