5f1b8d56c9670847e87f88b8587a2c34ccf764e9
Егоров Борис Владимирович <b.egorov@2gis.ru>
2020-05-07 13:07:58 +0700

    Stage 3/6: Enough coffee for everyone

--- src/machine/Main.kt
+++ src/machine/Main.kt
@@ -1,20 +1,47 @@
 package machine
 
-fun reportIngredientAmounts(numberOfCups: Int) {
-    val water = 200 * numberOfCups
-    val milk = 50 * numberOfCups
-    val beans = 15 * numberOfCups
+const val waterMlPerCup = 200
+const val milkMlPerCup = 50
+const val beansGPerCup = 15
 
-    println("For $numberOfCups of coffee you will need:")
-    println("$water ml of water")
-    println("$milk ml of milk")
-    println("$beans g of coffee beans")
+fun calculateNumberOfCupsForIngredients(waterMl: Int, milkMl: Int, beansG: Int): Int {
+    val numberOfCupsForGivenWater = waterMl / waterMlPerCup
+    val numberOfCupsForGivenMilk = milkMl / milkMlPerCup
+    val numberOfCupsForGivenBeans = beansG / beansGPerCup
+
+    return arrayListOf(numberOfCupsForGivenWater, numberOfCupsForGivenMilk, numberOfCupsForGivenBeans).min()!!
+}
+
+fun reportAbilityToMakeCoffee(numberOfCups: Int, numberOfCupsWhichCanBeMade: Int) {
+    when {
+        numberOfCups == numberOfCupsWhichCanBeMade ->
+            println("Yes, I can make that amount of coffee")
+        numberOfCups < numberOfCupsWhichCanBeMade -> {
+            val numberOfCupsOverAskedAmount = numberOfCupsWhichCanBeMade - numberOfCups
+            println("Yes, I can make that amount of coffee (and even $numberOfCupsOverAskedAmount more than that)")
+        }
+        numberOfCups > numberOfCupsWhichCanBeMade ->
+            println("No, I can make only $numberOfCupsWhichCanBeMade cups of coffee")
+    }
+}
+
+fun readInt(): Int {
+    return readLine()!!.toInt()
 }
 
 fun main() {
-    print("Write how many cups of coffee you will need: ")
+    print("Write how many ml of water the coffee machine has: ")
+    val waterMl = readInt()
 
+    print("Write how many ml of milk the coffee machine has: ")
+    val milkMl = readInt()
+
+    print("Write how many grams of coffee beans the coffee machine has: ")
+    val beansG = readInt()
+
+    print("Write how many cups of coffee you will need: ")
     val numberOfCups = readLine()!!.toInt()
 
-    reportIngredientAmounts(numberOfCups)
+    val numberOfCupsWhichCanBeMade = calculateNumberOfCupsForIngredients(waterMl, milkMl, beansG)
+    reportAbilityToMakeCoffee(numberOfCups, numberOfCupsWhichCanBeMade)
 }
